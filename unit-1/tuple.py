41fffcb742916d69f27074af69f7e94022ba8d5e
sanchitrathod07 <og.sanchit07@gmail.com>
2026-03-30 11:49:38 +0530

    Create tuple.py

---
+++ unit-1/tuple.py
@@ -0,0 +1,11 @@
+tup1 = (1, 2, 3)
+
+print("First element:", tup1[0])
+
+tup2 = (4, 5, (6, 7))
+print("Nested element:", tup2[2][0])
+
+print("Repetition:", tup1 * 2)
+
+tup3 = tup1 + tup2
+print("Concatenation:", tup3)
